a8a1bf487d14a29e716ee28398ece80a2411cf09
ami-GS <1991.daiki@gmail.com>
2016-02-17 16:30:22 +0900

    write detail of transport

--- transport.cc
+++ transport.cc
@@ -1,12 +1,44 @@
 #include <stdint.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 #include "transport.h"
 #include "frame.h"
 
 
-int64_t sendMessage(Message* m) {
-    return 0;
+Transport::Transport(const std::string targetIP, int targetPort) {
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    target.sin_family = AF_INET;
+    target.sin_port = htons(targetPort);
+    target.sin_addr.s_addr = inet_addr(targetIP.c_str());
 }
 
-int64_t readMessage(Message* m) {
-    return 0;
+int64_t Transport::sendMessage(Message* m) {
+    uint64_t len = m->GetWire(writeBuff);
+    if (len == -1) {
+        return -1;
+    }
+    write(sock, writeBuff, len);
+    return len;
+}
+
+int64_t Transport::readMessage(Message* m) {
+    memset(readBuff, 0, sizeof(readBuff));
+    int64_t status = read(sock, readBuff, sizeof(readBuff));
+
+    if (status > 0) {
+        /* Data read from the socket */
+        int64_t len = GetMessage(readBuff, m);
+        if (len == -1) {
+            return -1;
+        }
+    } else if (status == -1) {
+        /* Error, check errno, take action... */
+    } else if (status == 0) {
+        /* Peer closed the socket, finish the close */
+        close( sock );
+        /* Further processing... */
+    }
+
+    return status;
 }
--- transport.h
+++ transport.h
@@ -4,7 +4,15 @@
 #include <stdint.h>
 #include "frame.h"
 
-int64_t sendMessage(Message* m);
-int64_t readMessage(Message* m);
-
+class Transport {
+    int sock;
+    struct sockaddr_in target;
+    uint8_t readBuff[65535];
+    uint8_t writeBuff[65535];
+public:
+    Transport(const std::string targetIP, int targetPort);
+    ~Transport();
+    int64_t sendMessage(Message* m);
+    int64_t readMessage(Message* m);
+};
 #endif // MQTT_TRANSPORT_H_
