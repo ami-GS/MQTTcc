1667a07f01bf8f6e1ea8ba3b03eda203ccbed02e
ami-GS <1991.daiki@gmail.com>
2016-04-30 15:13:16 +0900

    fix memcmp location

--- MQTT_test.cc
+++ MQTT_test.cc
@@ -11,12 +11,12 @@ TEST(UtilTest, NormalTest) {
     int32_t enc_a_len = UTF8_encode(enc_a_wire, data);
     int32_t enc_e_len = data.size()+2;
     EXPECT_EQ(enc_e_len, enc_a_len);
-    EXPECT_TRUE(0 == std::memcmp(enc_a_wire, e_wire, enc_a_len));
+    EXPECT_TRUE(0 == memcmp(enc_a_wire, e_wire, enc_a_len));
 
     int64_t dec_a_len = 0;
     std::string dec_a_str = UTF8_decode(e_wire, &dec_a_len);
     EXPECT_EQ(enc_e_len, dec_a_len);
-    EXPECT_TRUE(0 == std::memcmp(data.c_str(), dec_a_str.c_str(), data.size()));
+    EXPECT_TRUE(0 == memcmp(data.c_str(), dec_a_str.c_str(), data.size()));
 
     int dataNum = 9;
     uint32_t lens[9] = {0, 1, 127, 128, 16383, 16384, 2097151, 2097152, 268435455};
@@ -26,7 +26,7 @@ TEST(UtilTest, NormalTest) {
     uint8_t e_wire_len[9] = {1, 1, 1, 2, 2, 3, 3, 4, 4};
     for (int i = 0; i < dataNum; i++) {
         int32_t remEnc_e_len = remainEncode(remEnc_a_wire, lens[i]);
-        EXPECT_TRUE(0 == std::memcmp(remEnc_e_wires[i], remEnc_a_wire, remEnc_e_len));
+        EXPECT_TRUE(0 == memcmp(remEnc_e_wires[i], remEnc_a_wire, remEnc_e_len));
     }
 
     int wire_progress = 0;
@@ -69,7 +69,7 @@ TEST(FrameHeaderTest, NormalTest) {
     uint8_t a_wire[30];
     int64_t a_len = fh->GetWire(a_wire);
     EXPECT_EQ(e_len, a_len);
-    EXPECT_TRUE(0 == std::memcmp(e_wire, a_wire, e_len));
+    EXPECT_TRUE(0 == memcmp(e_wire, a_wire, e_len));
 
     MQTT_ERROR a_err = NO_ERROR;
     FixedHeader* a_fh = new FixedHeader();
